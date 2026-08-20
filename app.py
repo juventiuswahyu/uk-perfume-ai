628184ef56ebaadbb7b8cbfb2872763e952f5411
juventiuswahyu <juventius.wahyu@unkartur.ac.id>
2026-08-20 14:40:27 +0700

    app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ if submitted:
                     model="openai/gpt-oss-20b",
                 )
                 
-                st.fireworks()
+                st.firework()
                 st.success("Analisis AI Investor Selesai!")
                 st.markdown(response.choices[0].message.content)
                 
